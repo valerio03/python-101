662e556797f822c96f96949d6b65ac826422f05b
naman kataria <126221662+valerio03@users.noreply.github.com>
2025-04-20 15:44:44 +0530

    Update 30_rocket.py

--- 6-functions/30_rocket.py
+++ 6-functions/30_rocket.py
@@ -3,6 +3,9 @@
 
 def distance_to_miles(distance):
   return distance / 1.609
-
+#if you want the user to add distance according to their will then add a function to input the distance in my case what i did was 
+#far=int(input('enter the distance in kilometer'))
+#answer = distance_to_miles(far)
+#print (answer)
 answer = distance_to_miles(10000)
 print(answer)
